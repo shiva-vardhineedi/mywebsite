c24a2e86ffe0403cbf73afc02925e809744bc512
Shiva Vardhineedi <shivavardhineedi@Shivas-MacBook-Pro.local>
2023-11-21 21:51:52 -0800

    .

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Shiva Vardhineedi',
     title: "Web Developer",
     desciption:"Graduate student at SJSU with 3 years experience in Full-Stack Develoment looking for Summer Intern Opportunities",
-    image: 'https://image.winudf.com/v2/image1/bmV0LndsbHBwci5pdGFjaGlfdWNoaWhhX3dhbGxwYXBlcl9zY3JlZW5fMF8xNjYxMTIzNzk5XzA4Mg/screen-0.webp?fakeurl=1&type=.webp',
+    image: '/portfolio.png',
     resumePdf: "/resume.pdf"
 }
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,10 +1,10 @@
 export const socialsData = {
-    github: 'https://github.com/',
+    github: 'https://github.com/shiva-vardhineedi',
     facebook: 'https://www.facebook.com/',
     linkedIn: 'https://www.linkedin.com/in/shiva-vardhineedi',
-    instagram: 'https://www.instagram.com/',
+    instagram: 'https://www.instagram.com/shiva_vardhineedi/',
     codepen: 'https://codepen.io/',
-    twitter: 'https://twitter.com/',
+    twitter: 'https://twitter.com/shvardhi',
     reddit: 'https://www.reddit.com/user/',
     blogger: 'https://www.blogger.com/',
     medium: 'https://medium.com/@',
